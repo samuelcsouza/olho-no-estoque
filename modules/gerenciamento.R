a98da6383ef84beee6b8a22b314f38dd2cd8c45a
samuelcsouza <samuel_dacruz@hotmail.com>
2023-10-08 14:33:23 -0300

    feat: list all products

--- modules/gerenciamento.R
+++ modules/gerenciamento.R
@@ -7,14 +7,162 @@ gerenciamento_ui <- function(id) {
 gerenciamento_server <- function(input, output, session, font_size) {
   ns <- session$ns
   
+  total_itens <- reactive({
+    
+    nrow(
+      list_products()
+    )
+    
+  })
+  
+  total_value <- reactive({
+    
+    list_products() %>% 
+      pull(value) %>% 
+      sum() %>% 
+      round(2) %>% 
+      paste0("R$ ", .)
+    
+  })
+  
+  
+  
+  products <- reactive({
+    
+    input$btn_update
+    
+    list_products()
+    
+  })
+  
+  output$table_product_list <- renderDataTable({
+    
+    .colnames <- c(
+      "ID",
+      "Material",
+      "Tratamento",
+      "Esférico",
+      "Cilíndrico",
+      "Marca",
+      "Valor"
+    )
+    
+    datatable(
+      products(),
+      colnames = .colnames,
+      rownames = TRUE,
+      options = list(searching = TRUE,
+                     paging = FALSE,
+                     info = FALSE,
+                     stripeClasses = FALSE,
+                     scrollX = TRUE,
+                     ordering = TRUE,
+                     scrollY = "350px")
+    )
+    
+  })
+  
+  
+  
+  
   output$manager_ui <- renderUI({
     
     font_size()
     
     fluidPage(
       
+      #### - - - - - - - - - - - - - - - - INFO CARDS - - - - - - - - - - - - - - - - ####
+      
+      div(
+        column(
+          width = 3
+        ),
+        
+        column(
+          width = 3,
+          infoBox(
+            title = accessible_text("Total Itens"),
+            value = accessible_text(total_itens()),
+            icon = icon('glasses'),
+            color = 'blue',
+            width = '100%'
+          )
+        ),
+        
+        column(
+          width = 3,
+          infoBox(
+            title = accessible_text("Total Estoque"),
+            value = accessible_text(total_value()),
+            icon = icon('dollar'),
+            color = 'blue',
+            width = '100%'
+          )
+        ),
+        
+        column(
+          width = 3
+        )
+        
+      ),
+      
+      br(),
+      
+      #### - - - - - - - - - - - - - - - - INFO BOX - - - - - - - - - - - - - - - - - ####
+      
       box(
-        title = FONT_SIZE
+        title = accessible_text("Produtos em estoque"),
+        solidHeader = TRUE,
+        width = 12,
+        status = "primary",
+        
+        
+        div(
+          dataTableOutput(
+            ns('table_product_list')
+          ),
+          style = paste0("font-size:", FONT_SIZE, "px;")
+        ),
+        
+        br(),
+        
+        div(
+          align = "right",
+          
+          actionButton(
+            inputId = ns("btn_create_product"),
+            label = accessible_text("Novo"),
+            icon = icon("plus"),
+            width = '15%'
+          ),
+          
+          actionButton(
+            inputId = ns("btn_update_product"),
+            label = accessible_text("Alterar"),
+            icon = icon("edit"),
+            width = '15%'
+          ),
+          
+          actionButton(
+            inputId = ns("btn_sell_product"),
+            label = accessible_text("Realizar Venda"),
+            icon = icon("check"),
+            width = '15%'
+          )
+          
+        ),
+        
+        
+        footer = div(
+          align = "center",
+          actionButton(
+            inputId = ns("btn_update"),
+            label = accessible_text("Atualizar"),
+            icon = icon("refresh"),
+            width = '35%'
+          )
+        )
+        
       )
       
     )
